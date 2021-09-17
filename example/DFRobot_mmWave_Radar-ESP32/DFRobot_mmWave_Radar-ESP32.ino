c8013904e9c556577dad2a933b80007fcab58e5c
DFRobot <yujie.hu@dfrobot.com>
2021-09-17 11:28:10 +0800

    first commit

---
+++ example/DFRobot_mmWave_Radar-ESP32/DFRobot_mmWave_Radar-ESP32.ino
@@ -0,0 +1,41 @@
+/*!
+   @file DFRobot_mmWave_Radar.ino
+   @ Read whether there is people or object moving in the detection range of the sensor. 
+   @ The sensor detection range and output delay time can be configured. Also you can restore the sensor to factory default settings. 
+   @n Experimental phenomenon: When the sensor starts successfully, 0 or 1 will be printed on the serial monitor. 
+   @ 0 means that there is no human or object moving in sensing area, 1 means the oppposite. 
+   @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+   @licence     The MIT License (MIT)
+   @author [huyujie](yujie.hu@dfrobot.com)
+   @version  V1.0
+   @date  2020-3-25
+   @https://github.com/DFRobot
+*/
+
+
+#include <DFRobot_mmWave_Radar.h>
+
+HardwareSerial mySerial(1);
+DFRobot_mmWave_Radar sensor(&mySerial);
+
+int ledPin = 13;
+
+void setup()
+{
+  Serial.begin(115200);
+  mySerial.begin(115200, SERIAL_8N1, 5, 18);//RX,TX
+  pinMode(ledPin, OUTPUT);
+  
+  sensor.factoryReset();    //Restore to the factory settings 
+  sensor.DetRangeCfg(0, 9);    //The detection range is as far as 9m
+  sensor.OutputLatency(0, 0);
+}
+
+void loop()
+{
+  int val = sensor.readPresenceDetection();
+  digitalWrite(ledPin, val);
+  Serial.println(val);
+
+
+}
